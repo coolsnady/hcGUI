52534033d3646fcd556850aec04cda2f1d2c54a4
Rocky <rainbow_jincheng@hotmail.com>
2018-07-13 11:34:39 +0800

    ticket charts data

--- app/selectors.js
+++ app/selectors.js
@@ -359,25 +359,57 @@ export const earnedStakingReward = createSelector(
   () => 6525094298
 );
 
+const ticketDataChartArray = []
 export const ticketDataChart = createSelector(
-  [transactions],
-  () => {
-    return [
-      { name: "23.10", immature: 4000, live: 2400, voted: 4000, legendName: "23.10.2017"},
-      { name: "24.10", immature: 3000, live: 1398, voted: 4000, legendName: "24.10.2017"},
-      { name: "25.10", immature: 2000, live: 7004, voted: 4000, legendName: "25.10.2017"},
-      { name: "26.10", immature: 2780, live: 3908, voted: 4000, legendName: "26.10.2017"},
-      { name: "27.10", immature: 1890, live: 4800, voted: 4000, legendName: "27.10.2017"},
-      { name: "28.10", immature: 2390, live: 3800, voted: 4000, legendName: "28.10.2017"},
-      { name: "29.10", immature: 3490, live: 4300, voted: 4000, legendName: "29.10.2017"},
-      { name: "30.10", immature: 3490, live: 4300, voted: 4000, legendName: "30.10.2017"},
-      { name: "01.11", immature: 3490, live: 4300, voted: 4000, legendName: "01.11.2017"},
-      { name: "02.11", immature: 3490, live: 4300, voted: 4000, legendName: "02.11.2017"},
-      { name: "03.11", immature: 3490, live: 4300, voted: 4000, legendName: "03.11.2017"},
-      { name: "04.11", immature: 3490, live: 4300, voted: 4000, legendName: "04.11.2017"},
-      { name: "05.11", immature: 3490, live: 4300, voted: 4000, legendName: "05.11.2017"},
-      { name: "06.11", immature: 3490, live: 4300, voted: 4000, legendName: "06.11.2017"},
-    ];
+  [transactions, getState,currentBlockHeight],
+  (transactions, state,currentBlockHeight) => {
+    var immatureCount =0 ;
+    var liveCount =0 ;
+    var votedCount =0 ;
+    var currHeight = currentBlockHeight;
+    
+    var ticketMaturity = chainParams(state).TicketMaturity;
+
+    transactions.forEach(a=>{
+      if(a.txType === "Vote") {
+        votedCount++;
+      } else if(a.txType === "Ticket" ) 
+      {
+       
+        if(a.txHeight < 0){
+          return
+        }
+        if( a.txHeight+ ticketMaturity < currHeight){
+          
+          liveCount++;
+        } else {
+          immatureCount++;
+        }
+      } else {
+        //others abort
+      }
+    });
+
+    let lastIndex = ticketDataChartArray.length - 1 || 0
+
+    if(ticketDataChartArray[lastIndex]
+      && numberEqual(ticketDataChartArray[lastIndex].immature , immatureCount)
+      && numberEqual(ticketDataChartArray[lastIndex].live , liveCount)
+      && numberEqual(ticketDataChartArray[lastIndex].voted , votedCount)) {
+
+      return ticketDataChartArray;
+    }
+    ticketDataChartArray.push({
+        name: new Date().toTimeString(), 
+        immature: immatureCount, 
+        live: liveCount, 
+        voted: votedCount, 
+        legendName: new Date().toDateString()
+      });
+    if(ticketDataChartArray.length>15) {
+      ticketDataChartArray.shift();
+    }
+    return ticketDataChartArray;
   }
 );
 
@@ -784,49 +816,4 @@ const numberEqual =  (left, right) =>{
   return Math.abs(left - right) < Number.EPSILON * Math.pow(2, 2);
 }
 
-
-/*
-const ticketDataChartArray = []
-export const ticketDataChart = createSelector(
-  [transactions, network,requiredStakepoolAPIVersion],
-  (transactions, network,requiredStakepoolAPIVersion) => {
-    var immatureCount =0 ;
-    var liveCount =0 ;
-    var votedCount =0 ;
-    var currHeight = currentBlockHeightV;
-    var ticketMaturity = chainParams(network).TicketMaturity;
-    transactions.forEach(a=>{
-      if(a.txType === "Vote") {
-        votedCount++;
-      } else if(a.txType === "Ticket" ) 
-      {
-        if( a.txHeight+ ticketMaturity < currHeight){
-          liveCount++;
-        } else {
-          immatureCount++;
-        }
-      } else {
-        //others abort
-      }
-    });
-
-    if(ticketDataChartArray[0]
-      && numberEqual(ticketDataChartArray[0].immature , immatureCount)
-      && numberEqual(ticketDataChartArray[0].live , liveCount)
-      && numberEqual(ticketDataChartArray[0].voted , votedCount)) {
-      return spendableAndLockedBalanceArray;
-    }
-    ticketDataChartArray.push({
-        name: new Date().toTimeString(), 
-        immature: immatureCount, 
-        live: liveCount, 
-        voted: votedCount, 
-        legendName: new Date().toDateString()
-      });
-    if(ticketDataChartArray.length>15) {
-      ticketDataChartArray.shift();
-    }
-    return ticketDataChartArray;
-  }
-);
-*/
+const getState = state=>state
